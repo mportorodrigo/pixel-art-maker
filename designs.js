3a22d4093de00111620fbb366c2914adbfe660ea
Rodrigo Porto <mportorodrigo@outlook.com>
2019-06-10 20:07:04 -0300

    feat: Add variables to store user input
    Adds variavels to store the height and width for the grid and color,
    defined by the user. They will be declared as let as necessary.

--- designs.js
+++ designs.js
@@ -1,10 +1,14 @@
 // Select color input
+const selectedColor = document.getElementById('colorPicker');
+
 // Select size input
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
 
 }
